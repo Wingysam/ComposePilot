208c3f868c5f2144bde96c037a41eebe478cbf2a
Wingy <git@wingysam.xyz>
2024-09-21 18:15:07 -0400

    clean up .bun-build files

--- build.sh
+++ build.sh
@@ -3,8 +3,11 @@ set -eu
 
 build() {
     arch=$1
-    bun build src/index.ts --compile --minify --sourcemap --outfile "dist/$arch" --target "$arch"
+    bun build src/index.ts --compile --minify --sourcemap --outfile "dist/composepilot-$arch" --target "$arch"
 }
+
+rm -rf dist
+mkdir dist
 
 build bun-linux-x64
 build bun-linux-x64-baseline
@@ -18,3 +21,8 @@ build bun-darwin-x64
 build bun-darwin-x64-baseline
 
 build bun-darwin-arm64
+
+# Bun is meant to clean these up, but doesn't always:
+# https://github.com/oven-sh/bun/issues/14020
+# The issue appears to be specific to darwin arm64 builds.
+rm -f ./.*.bun-build
